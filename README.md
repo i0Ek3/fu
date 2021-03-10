67a3a097dcf9d17f1194fe0331d14d07d69631f1
i0Ek3 <kno30826@gmail.com>
2021-03-10 22:39:10 +0800

    Update README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# fu
+
+> Yes, fu means that word, f\*\*k you!
+
+Actually `fu` is a model to train the data and output wrong result, so I named it fu.
+
+## Points
+
+soon...
---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import fu
+
+
+def main():
+    anw = input("Can you translate me?")
+    if anw:
+        print("Are you pig?")
+    fu.fu()
+
+if __name__ == '__main__':
+    main()
